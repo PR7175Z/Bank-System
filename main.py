3a73081b3f3783ee5f463e2f27f4d9b0e1c4468a
PR7175Z <aadimpraz@gmail.com>
2024-03-20 23:01:45 +0545

    added switch case

--- main.py
+++ main.py
@@ -7,18 +7,18 @@ def get_info():
     name = input("Enter name: ")
     username = input(f"Enter {name}'s username: ")
     password = input(f"Enter {name}'s password: ")
-    blc = input(f"Enter {name}'s balance: ")
+    blc = int(input(f"Enter {name}'s balance: "))
     obj = account(name, username, password, blc)
     return obj
 
 def check_login(userinfo):
-    os.system('cls')
     username = input("Enter username: ")
     password = getpass.getpass("Enter password: ")
     for x in userinfo:
         if username == x.username and password == x.password:
             return userinfo.index(x)
             # print(userinfo.index(x))
+        
 
 class account(object):
     def __init__(self, name, username, password, balance):
@@ -33,11 +33,55 @@ class account(object):
     def deposit(self, amount):
         self.balance += amount  
 
+    def sendmoney(self, receiver_username, amount, userdb):
+        # self.balance -= amount
+        for x in userdb:
+            if receiver_username == x.username:
+                self.balance = self.balance - amount
+                x = userdb.index(x)
+                userdb[x].balance = userdb[x].balance + amount
+                # print('user exist')
+                return x
+            else:
+                print('no user')
+        # receiver_username.balance += amount
+
     def showval(self):
         return f'Name: {self.name} \nBalance: {self.balance}'
+    
+program = True
+usersinfo = []
+while program:
+    print("1.Add user\n2.login\n3.Exit")
+    fchoice = int(input("Enter your choice: "))
+    match fchoice:
+        case 1:
+            os.system('cls')
+            usersinfo.append(get_info())
+            # print('add user')
+        case 2: 
+            os.system('cls')
+            a = check_login(usersinfo)
+            try:
+                print(usersinfo[a].showval())
+            except:
+                print("There is error in login")
+        case 3:
+            program = False
+            print('exit')
 
-n = int(input('Enter number of new accounts'))
-usersinfo = [get_info() for x in range(0,n)]
+# n = int(input('Enter number of new accounts'))
+# usersinfo = [get_info() for x in range(0,n)]
 
-a = check_login(usersinfo)
-print(usersinfo[a].showval())
+# a = check_login(usersinfo)
+# try:
+#     print(usersinfo[a].showval())
+# except:
+#     print("There is error in login")
+# # sendmoney
+# x = usersinfo[a].sendmoney('admin', 200, usersinfo)
+# try:
+#     print(usersinfo[a].showval())
+#     print(usersinfo[x].name, usersinfo[x].username, usersinfo[x].balance)
+# except:
+#     print("There is error in login")
